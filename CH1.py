c95df2d9ff1457727b0aa174a8ee8eea66b8e174
BrianChhuoy <122625946+BrianChhuoy@users.noreply.github.com>
2023-02-20 17:12:16 -0600

    Add files via upload

---
+++ CH1.py
@@ -0,0 +1,48 @@
+#Ch 1 Testeroo
+#This Below Shows the end='' Function
+"""
+print("Hello World", end=' ') #Notice how the end='' will make the sentence from line 5 print on line 4
+print(" NO Fukc u D:")
+print("HI im brain",end=' ') # - However line 6 will print on line 5 so their is no space in the function
+print("Whats good" )
+print("YEs")
+"""
+#Shows input
+"""
+name = input("Howdy their fine gentleman what is your name?:")
+name
+print("FUck u",name)
+"""
+
+#The Different types of functions
+
+#print("Hi :D ") #Simple Print
+"""
+#int = no decimals
+cookies= int(input("How many cookies have you ate? :")) #Notice how you are only allowed to use numbers no decimal
+cookies
+print("You have ate",cookies, "of cookies")
+"""
+#in this you are not allowed to use anything but numbers even if you don't use a decimal it will force one
+#so if u print 12 -> 12.0
+"""
+days= float(input("How many hours have you spend outside? :"))
+days
+print("You have spent",days,"days outside")
+"""
+#With input you can use any thing decimals, letters, numbers
+"""
+kill= input("Enter the name of the target you want assassinated:")
+kill
+print("Your target is",kill,".")
+"""
+#The + Function Will glue together 2 strings
+#print("hello" + "Good Bye")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Excercise 3
+#1.) Unless you type print("Greetings!") nothing will print
+#2.)
+#name= input("what is your name?:")
+#3.)It is a script designed to be run directly
+#4. It will recieve the code then check for syntax errors afterwards it will run
+
---
+++ Chapter 1 Excercises.py
@@ -0,0 +1,68 @@
+#******* WORK ON YOUR OWN TIME THE REST IS BORING
+#Chater 1-1b Excercise
+#1.) - Cpu, Memory, Input/Output Device
+#2.) -
+#Chapter 1-6d Chapter Review
+#1.)
+"""
+8
+8 * 2
+8 ** 2
+8/12
+8 // 12
+8/0
+"""
+#2.)
+"""
+name= input("What is your name?:")
+address= input("What is your home address?:")
+phone=input("What is your phone number?: ")
+print(name,address,phone)
+"""
+"""
+#3.)
+name= input("Please enter your name:")
+print("Your name is",name)
+"""
+#4.)
+"""
+width= int(input("Enter with width: "))
+height= int(input("Enter with height: "))
+area= width* height
+print("The area is",area,"square units.")
+"""
+#5.)
+"""
+width= int(input("Enter the first leg: "))
+height= int(input("Enter the second leg: "))
+area= width* height*.5
+print("The area of your triangle is",area,"square units.")
+"""
+#6.)
+"""
+radius= int(input("Enter the radius of the circle:"))
+area=((radius * 3.14)**2)
+print(area)
+"""
+#7.)
+"""
+name= input("Enter you name:")
+age=int(input("Enter your age:"))
+print("Hi",name,"your age is",age,"years old.")
+"""
+#8.)
+"""
+number= int(input("Enter a number:"))
+number+=1
+print(number)
+#What happened in this was the input had you put a integer then it would add 1 to the integer afterward world print the new number 
+"""
+#9.)
+"""
+input("What is your name?:")
+#essentially they asked you take and they store 
+"""
+#10.)
+"""
+help()
+"""
