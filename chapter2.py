18777f3e38d4a5a44337e72e7825f9baa3e25f0e
BrianChhuoy <122625946+BrianChhuoy@users.noreply.github.com>
2023-02-21 16:37:14 -0600

    Add files via upload

---
+++ chapter2.py
@@ -0,0 +1,27 @@
+#Chapter 2
+#The Below Example shows how the triple quote can make print function print on different lines
+"""
+print('''HELLO THEIR MY
+ NAME IS 
+BRIAN''')
+
+"""
+#The below example shows the "*" operator it will print "a" 10 times. Making aaaaaaaaaaaaburger
+#print ("a"*10 + "burger")
+
+#--------------------------------------------------------------------
+#Test on whether you can write on the left side
+"""
+sam= input("hi my name is sam what is yours?")
+print(sam)
+input("hi my name is sam what is yours?")=sam #in this statement it is shown that you can't put the variable name on the right side so it must be on left 
+print(sam)
+"""
+#The below example shows how a variable is assigned
+"""
+name= 'Brian'
+age='18'
+gender= 'male'
+allmight = (name+" "+ age+ " "+ gender)
+print(allmight)
+"""
